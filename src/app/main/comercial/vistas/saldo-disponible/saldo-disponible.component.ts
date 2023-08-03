db678a10968abe014eb3b05f7c34f59de4c3823a
jimmy <rootkaliente@gmail.com>
2023-08-03 09:20:49 -0500

    [Jimmy] modificar el filtro de listar el saldo disponible

--- src/app/main/comercial/vistas/saldo-disponible/saldo-disponible.component.ts
+++ src/app/main/comercial/vistas/saldo-disponible/saldo-disponible.component.ts
@@ -1,28 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ViewChild, ViewEncapsulation} from '@angular/core';
 import {SaldoContableService} from '../saldo-contable/saldo-contable.service';
+import {NgbPagination} from '@ng-bootstrap/ng-bootstrap';
+import {Subject} from 'rxjs';
 
 @Component({
   selector: 'app-saldo-disponible',
   templateUrl: './saldo-disponible.component.html',
-  styleUrls: ['./saldo-disponible.component.scss']
+  styleUrls: ['./saldo-disponible.component.scss'],
+  encapsulation: ViewEncapsulation.None,
+  host: { class: 'ecommerce-application' }
 })
 export class SaldoDisponibleComponent implements OnInit {
+  @ViewChild(NgbPagination) paginator: NgbPagination;
 
   public page_size: any = 10;
   public collectionSize;
   public page = 1;
   public cliente;
   public notasPedido = [];
+  private _unsubscribeAll: Subject<any>;
 
   constructor(
       private _consultaCreditosService: SaldoContableService,
-  ) { }
+  ) {
+    this._unsubscribeAll = new Subject();
+  }
 
   ngOnInit(): void {
-    this._consultaCreditosService.listarEnvios({page_size: this.page_size, page: this.page, estado: ['Aprobado']})
-        .subscribe((data) => {
-          this.notasPedido = data.info;
-        });
+    this.refrescarLista();
   }
 
   datosCliente(cliente) {
@@ -31,9 +36,15 @@ export class SaldoDisponibleComponent implements OnInit {
   }
 
   refrescarLista() {
-    this._consultaCreditosService.listarEnvios({page_size: this.page_size, page: this.page, estado: ['Aprobado']})
+    this._consultaCreditosService.listarEnvios({page_size: this.page_size, page: this.page - 1, estado: ['Aprobado']})
         .subscribe((data) => {
+          this.collectionSize = data.cont;
           this.notasPedido = data.info;
         });
+  }
+  ngOnDestroy(): void {
+    // Unsubscribe from all subscriptions
+    this._unsubscribeAll.next();
+    this._unsubscribeAll.complete();
   }
 }

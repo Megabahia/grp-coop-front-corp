dd6b9d27fefeec4d2a0a6a04e531f48896cd9995
jimmy <rootkaliente@gmail.com>
2023-08-24 16:30:57 -0500

    [Jimmy] modificar las imagenes en nabvar

--- src/app/menu/menu.ts
+++ src/app/menu/menu.ts
@@ -163,119 +163,119 @@ export const menu: CoreMenu[] = [
       },
     ]
   },
-  {
-    id: 'bigPuntos',
-    title: 'BIG PUNTOS',
-    // role: [Role.SuperMonedas],
-    // translate: 'MENU.PAGES.SECTION',
-    type: 'collapsible',
-    icon: 'dollar-sign',
-    children: [
-      {
-        id: 'cobrarConSuperMonedas',
-        title: 'Cobrar con Super Monedas',
-        // translate: 'MENU.APPS.EMAIL',
-        type: 'item',
-        icon: 'users',
-        url: '/bigpuntos/cobrar'
-      },
-      {
-        id: 'reporteCobrosConSupermonedas',
-        title: 'Reporte de Cobros con Supermonedas',
-        // translate: 'MENU.APPS.EMAIL',
-        type: 'item',
-        icon: 'users',
-        url: '/bigpuntos/reporteCobros'
-      },
-      {
-        id: 'clientes',
-        title: 'Clientes',
-        // translate: 'MENU.APPS.EMAIL',
-        type: 'item',
-        icon: 'users',
-        url: 'personas/supermonedas/mis-monedas'
-      },
-      {
-        id: 'productos',
-        title: 'Productos',
-        // translate: 'MENU.APPS.EMAIL',
-        type: 'item',
-        icon: 'shopping-bag',
-        url: 'personas/supermonedas/mis-monedas'
-      },
-      {
-        id: 'cargarMonedas',
-        title: 'Cargar Big Puntos',
-        // translate: 'MENU.APPS.EMAIL',
-        type: 'item',
-        icon: 'dollar-sign',
-        url: 'corp/cargar-bigpuntos'
-      },
-      // {
-      //   id: 'cargarPremios',
-      //   title: 'Cargar Premios',
-      //   // translate: 'MENU.APPS.EMAIL',
-      //   type: 'item',
-      //   icon: 'package',
-      //   url: 'personas/supermonedas/mis-monedas'
-      // },
-      {
-        id: 'cargarPremios',
-        title: 'Cargar Premios',
-        // translate: 'MENU.APPS.EMAIL',
-        type: 'item',
-        icon: 'package',
-        url: 'personas/supermonedas/mis-monedas'
-      }, {
-        id: 'mdoMotorOfertas',
-        title: 'MDO Motor de Ofertas',
-        // translate: 'MENU.PAGES.SECTION',
-        type: 'collapsible',
-        icon: 'file-text',
-        children: [
-          {
-            id: 'parametrizacion',
-            title: 'Parametrización',
-            // translate: 'MENU.APPS.EMAIL',
-            type: 'item',
-            icon: 'circle',
-            url: 'personas/supermonedas/mis-monedas'
-          },
-          {
-            id: 'prediccionesCrosseling',
-            title: 'Predicciones para crosseling',
-            // translate: 'MENU.APPS.EMAIL',
-            type: 'item',
-            icon: 'circle',
-            url: 'personas/supermonedas/mis-monedas'
-          },
-          {
-            id: 'prediccionesRefil',
-            title: 'Predicciones para refil',
-            // translate: 'MENU.APPS.EMAIL',
-            type: 'item',
-            icon: 'circle',
-            url: 'personas/supermonedas/mis-monedas'
-          },
-          {
-            id: 'prediccionesNuevosProductos',
-            title: 'Predicciones para nuevos productos',
-            // translate: 'MENU.APPS.EMAIL',
-            type: 'item',
-            icon: 'circle',
-            url: 'personas/supermonedas/mis-monedas'
-          },
-          {
-            id: 'generarOfertaCliente',
-            title: 'Generar oferta para cliente',
-            // translate: 'MENU.APPS.EMAIL',
-            type: 'item',
-            icon: 'circle',
-            url: 'personas/supermonedas/mis-monedas'
-          }]
-      }
-    ]
-  },
+  // {
+  //   id: 'bigPuntos',
+  //   title: 'BIG PUNTOS',
+  //   // role: [Role.SuperMonedas],
+  //   // translate: 'MENU.PAGES.SECTION',
+  //   type: 'collapsible',
+  //   icon: 'dollar-sign',
+  //   children: [
+  //     {
+  //       id: 'cobrarConSuperMonedas',
+  //       title: 'Cobrar con Super Monedas',
+  //       // translate: 'MENU.APPS.EMAIL',
+  //       type: 'item',
+  //       icon: 'users',
+  //       url: '/bigpuntos/cobrar'
+  //     },
+  //     {
+  //       id: 'reporteCobrosConSupermonedas',
+  //       title: 'Reporte de Cobros con Supermonedas',
+  //       // translate: 'MENU.APPS.EMAIL',
+  //       type: 'item',
+  //       icon: 'users',
+  //       url: '/bigpuntos/reporteCobros'
+  //     },
+  //     {
+  //       id: 'clientes',
+  //       title: 'Clientes',
+  //       // translate: 'MENU.APPS.EMAIL',
+  //       type: 'item',
+  //       icon: 'users',
+  //       url: 'personas/supermonedas/mis-monedas'
+  //     },
+  //     {
+  //       id: 'productos',
+  //       title: 'Productos',
+  //       // translate: 'MENU.APPS.EMAIL',
+  //       type: 'item',
+  //       icon: 'shopping-bag',
+  //       url: 'personas/supermonedas/mis-monedas'
+  //     },
+  //     {
+  //       id: 'cargarMonedas',
+  //       title: 'Cargar Big Puntos',
+  //       // translate: 'MENU.APPS.EMAIL',
+  //       type: 'item',
+  //       icon: 'dollar-sign',
+  //       url: 'corp/cargar-bigpuntos'
+  //     },
+  //     // {
+  //     //   id: 'cargarPremios',
+  //     //   title: 'Cargar Premios',
+  //     //   // translate: 'MENU.APPS.EMAIL',
+  //     //   type: 'item',
+  //     //   icon: 'package',
+  //     //   url: 'personas/supermonedas/mis-monedas'
+  //     // },
+  //     {
+  //       id: 'cargarPremios',
+  //       title: 'Cargar Premios',
+  //       // translate: 'MENU.APPS.EMAIL',
+  //       type: 'item',
+  //       icon: 'package',
+  //       url: 'personas/supermonedas/mis-monedas'
+  //     }, {
+  //       id: 'mdoMotorOfertas',
+  //       title: 'MDO Motor de Ofertas',
+  //       // translate: 'MENU.PAGES.SECTION',
+  //       type: 'collapsible',
+  //       icon: 'file-text',
+  //       children: [
+  //         {
+  //           id: 'parametrizacion',
+  //           title: 'Parametrización',
+  //           // translate: 'MENU.APPS.EMAIL',
+  //           type: 'item',
+  //           icon: 'circle',
+  //           url: 'personas/supermonedas/mis-monedas'
+  //         },
+  //         {
+  //           id: 'prediccionesCrosseling',
+  //           title: 'Predicciones para crosseling',
+  //           // translate: 'MENU.APPS.EMAIL',
+  //           type: 'item',
+  //           icon: 'circle',
+  //           url: 'personas/supermonedas/mis-monedas'
+  //         },
+  //         {
+  //           id: 'prediccionesRefil',
+  //           title: 'Predicciones para refil',
+  //           // translate: 'MENU.APPS.EMAIL',
+  //           type: 'item',
+  //           icon: 'circle',
+  //           url: 'personas/supermonedas/mis-monedas'
+  //         },
+  //         {
+  //           id: 'prediccionesNuevosProductos',
+  //           title: 'Predicciones para nuevos productos',
+  //           // translate: 'MENU.APPS.EMAIL',
+  //           type: 'item',
+  //           icon: 'circle',
+  //           url: 'personas/supermonedas/mis-monedas'
+  //         },
+  //         {
+  //           id: 'generarOfertaCliente',
+  //           title: 'Generar oferta para cliente',
+  //           // translate: 'MENU.APPS.EMAIL',
+  //           type: 'item',
+  //           icon: 'circle',
+  //           url: 'personas/supermonedas/mis-monedas'
+  //         }]
+  //     }
+  //   ]
+  // },
   {
     id: 'comercial',
     title: 'Comercial',
